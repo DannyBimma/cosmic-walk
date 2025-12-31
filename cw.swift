818bca130945626739b70d212793ed81a2a3772d
DannyBimma <dantrotman92@gmail.com>
2025-12-31 14:31:54 -0400

    Init files and data

---
+++ cw.swift
@@ -0,0 +1,11 @@
+/*
+* Routine:  A program that calculates the simulated distance traveled
+            by the user--based on their walking data--towards various
+            celestial bodies.
+
+* Author:   DannyBimma
+
+* Date:     December, 2025.
+
+* Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+*/
